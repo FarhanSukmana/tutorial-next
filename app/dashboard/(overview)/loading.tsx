c4f0be8a0a8e82ac379ae8679c308cc253c15f03
Muhammad Farhan Sukmana <sukmanafarhan2@gmail.com>
2024-03-02 23:29:58 +0700

    Push ke-2

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardSkeeton from '@/app/ui/skeletons';
+
+const loading = () => {
+  return (
+    <div><DashboardSkeeton /></div>
+  )
+}
+
+export default loading
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import SideNav from '@/app/ui/dashboard/sidenav'
-import {inter} from '@/app/ui/fonts'
+import SideNav from '@/app/ui/dashboard/sidenav';
+import { inter } from '@/app/ui/fonts';
 
-const layout = ({children} : {children:React.ReactNode}) => {
-return (
-    <div className='flex h-screeen flex-col md:flex-row md:overflow_hidden'>
-        <div className='w-full flex-none md:w-64'>
-            <SideNav/>
-        </div>
-        <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screeen md:overflow_hidden flex flex-col md:flex-row">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
-)
-}
+  );
+};
 
-export default layout
+export default layout;
